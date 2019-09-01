5e37903c78cfa606bcadffc3fb0fd355fc18ac7b
zhaoyang <980355088@qq.com>
2019-09-01 18:21:02 +0800

    update

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-let router = require('./routes');
+const bodyParser = require('koa-bodyparser'); // koa上下文的formData数据解析到ctx.request.body中
+let router = require('./src/routes');
+
 const app = new Koa();
 
 app.use(bodyParser());
-
 app
   .use(router.routes())
   .use(router.allowedMethods());
-// app.use(async ctx => {
-//     ctx.body = 'Hello koa';
-// })
 
 app.listen(8888, () => {
     console.log('http://localhost:8888');
