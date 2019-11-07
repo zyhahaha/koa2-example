9d5035ce725cef206b0769bacdeef9f6add53628
zhaoyang <18355403288@163.com>
2019-11-07 16:20:51 +0800

    update

--- config/index.js
+++ config/index.js
@@ -3,9 +3,9 @@ const config = {
   port: 8888,
   // 数据库配置
   database: {
-    host: '47.96.234.53',
-    user: 'root',
-    password: '',
+    host: '180.76.103.151',
+    user: 'zyhahaha',
+    password: 'zy@980355088',
     database: 'koa_test',
     port: 3306,
     multipleStatements: true // 允许多条sql同时执行
