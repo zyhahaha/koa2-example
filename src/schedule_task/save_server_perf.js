e7e419f17a3e52bf8ec4d534f66fbd7c0d624035
zhaoyang <980355088@qq.com>
2021-08-27 15:25:58 +0800

    update

--- src/schedule_task/save_server_perf.js
+++ src/schedule_task/save_server_perf.js
@@ -1,6 +1,6 @@
 const os = require('os');
 const Router = require('koa-router');
-const db = require('../db');
+const db = require('../db/mysql');
 const SERVER_PERF_DB = 'server_perf';
 const LENGTH = os.cpus().length;
 
